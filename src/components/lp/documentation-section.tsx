1f61fd11378aa5ed868f48874e865f8f9213f04b
Henrique Ataide <henriqueluiz16022000@gmail.com>
2026-04-12 13:52:53 -0300

    hotifix

--- src/components/lp/documentation-section.tsx
+++ src/components/lp/documentation-section.tsx
@@ -67,12 +67,10 @@ export default function DocumentationSection() {
             text="Platform Features"
           />
           <div className="self-stretch text-center flex justify-center flex-col text-[#49423D] text-3xl md:text-5xl font-semibold leading-tight md:leading-[60px] font-sans tracking-tight">
-            Streamline your business operations
+            Uma plataforma pensada para a operação contábil
           </div>
           <div className="self-stretch text-center text-[#605A57] text-base font-normal leading-7 font-sans">
-            Manage schedules, analyze data, and collaborate with your team
-            <br />
-            all in one powerful platform.
+            Estruture workflows, monitore o andamento dos processos e mantenha equipe e clientes informados com uma experiência muito mais clara do início ao fim.
           </div>
         </div>
       </div>
